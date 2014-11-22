e4d27e052fd9f5d12c85a68056c132ea263622f8
Secondcow2die <Amzi@Amzis-MacBook-Pro.local>
2014-11-21 19:55:27 -0600

    This is MINE (amzi) ALL mine.

--- src/musicApp/Octave.java
+++ src/musicApp/Octave.java
@@ -1,5 +1,100 @@
+
 package musicApp;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class Octave {
+	
+	// The current octave you are on.
+	String octave;
+	int totalSfiles;
+	
+	HashMap<String, Clip> sounds;
+	
+	
+	/**********************
+	 * Constructor
+	 *********************/
+	public Octave(){
+		
+		totalSfiles = 0;
+		
+		this.sounds = new HashMap<String, Clip>();
+		
+		try 
+		{
+			this.loadSound("Key A", new File(Octave.class.getResource("/Key A.wav").toURI()));
+			this.loadSound("Key B", new File(Octave.class.getResource("/Key B.wav").toURI()));
+			this.loadSound("Key C", new File(Octave.class.getResource("/Key C.wav").toURI()));
+			this.loadSound("Key D", new File(Octave.class.getResource("/Key D.wav").toURI()));
+			this.loadSound("Key E", new File(Octave.class.getResource("/Key E.wav").toURI()));
+			this.loadSound("Key F", new File(Octave.class.getResource("/Key F.wav").toURI()));
+			this.loadSound("Key G", new File(Octave.class.getResource("/Key G.wav").toURI()));
+		} 
+		catch (URISyntaxException e) 
+		{
+			// unable to load sounds
+			e.printStackTrace();
+		}
+		
+		// Create an audiostream from the inputstream
+	
+	}
+	
+	/****************************
+	 * Accessor for current Octive.
+	 ***************************/
+	public String getOctave()
+	{
+		return octave;
+	}
+	/****************************
+	 * Mutator for sound files.
+	 ***************************/
+	public void setOctave(String o)
+	{
+		octave = o;
+	}
+	
+	public void loadSound(String soundName, File soundFile)
+	{
+		try 
+		{
+			this.sounds.put(soundName, AudioSystem.getClip()); //get clip gets a new clip from the system
+			this.sounds.get(soundName).open(AudioSystem.getAudioInputStream(soundFile)); //opens the sound file with the clip class
+		} 
+		catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) 
+		{
+			//error loading sound
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void playNote(String soundName)
+	{
+		try
+		{
+			this.sounds.get(soundName).start();
+		}
+		catch(Exception e)
+		{
+			//file does not exist
+		}
+	}
+	
+	
+	
+	
 
 }
+	
+	
--- src/musicApp/SoundBank.java
+++ src/musicApp/SoundBank.java
@@ -1,28 +1,49 @@
 package musicApp;
 
-public class SoundBank
-{
-	//These arrays are parallel
-	Notes soundFiles[];
+import java.io.File;
+import java.net.URISyntaxException;
+
+public class SoundBank {
+	// These arrays are parallel
+	Octave octaveList[];
 	String soundTypes[];
-	
-	public Notes[] getSoundFiles()
-	{
-		return soundFiles;
+
+	public SoundBank() {
+
+		/****************************************************
+		 * In here i'd like to store the sounds in an array.
+		 ****************************************************/
+
+		octaveList = new Octave[6];
+		
+		for (char i = 'A'; i < 'G'; i++) {
+			
+			octaveList[((int) i) - 65] = new Octave();
+
+			try {
+				octaveList[((int) i) - 65].loadSound("Key " + i, new File(Octave.class
+						.getResource("/Key " + i + ".wav").toURI()));
+			} catch (URISyntaxException e) {
+
+				e.printStackTrace();
+			}
+		}
+
 	}
-	
-	public String[] getSoundTypes()
-	{
+
+	public Octave[] getOctave() {
+		return octaveList;
+	}
+
+	public String[] getSoundTypes() {
 		return soundTypes;
 	}
-	
-	public void setSoundFiles(Notes sf[])
-	{
-		soundFiles = sf;
+
+	public void setOctive(Octave sf[]) {
+		octaveList = sf;
 	}
-	
-	public void setSoundTypes(String st[])
-	{
+
+	public void setSoundTypes(String st[]) {
 		soundTypes = st;
 	}
 }
